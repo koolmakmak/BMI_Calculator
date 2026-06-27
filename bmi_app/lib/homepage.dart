2dab513625945dccda19cb6136ecbbe45dc74a69
koolmakmak <66991041@kmitl.ac.th>
2026-06-27 19:03:23 +0700

    fix: Chen image and add thickness of border

--- bmi_app/lib/homepage.dart
+++ bmi_app/lib/homepage.dart
@@ -22,13 +22,31 @@ class _HomePageState extends State<HomePage> {
               style: TextStyle(fontSize: 80, color: Colors.white),
             ),
             const SizedBox(height: 60),
-            Image.asset('assets/images/Chen1.jpg', width: 250, height: 250, fit: BoxFit.cover),
+            Container(
+              width: 250,
+              height: 250,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(300),
+                border: Border.all(
+                  color: Color.fromRGBO(209, 194, 127, 1),
+                  width: 10,
+                ),
+                image: const DecorationImage(
+                  image: AssetImage('assets/images/Chen1.jpg'),
+                  fit: BoxFit.cover,
+                )
+              ),
+            ),
             const SizedBox(height: 60),
-            ElevatedButton(onPressed: () {
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromRGBO(209, 194, 127, 1),
+              )
+              ,onPressed: () {
               Navigator.push(
                 context, 
                 MaterialPageRoute(builder: (context) => const CalculatePage()),);
-            }, child: const Text('Calculate'))
+            }, child: const Text('Calculate', style: TextStyle(color: Colors.black),))
           ],
         ),
       )
